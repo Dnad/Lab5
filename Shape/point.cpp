4d6f2cc79ca7697a098b4fd70bbee1d857636291
Dnad <dnad888@gmail.com>
2015-03-13 10:20:20 -0400

    test point
    verifie si les coordonne sont negative
    verifie si les points sont identique ou different

--- Shape/point.cpp
+++ Shape/point.cpp
@@ -9,17 +9,23 @@ Point::Point( double _x, double _y ) :
 	x(_x), 
 	y(_y)
 {	
-	
+	if (x < 0 || y < 0){
+		throw invalid_argument("Le nombre ne peut pas etre negatif");
+	}
+
 }
 
 bool Point::operator == (const Point & point) const
 {
-	throw logic_error("Not Implmented yet");
-	return true;
+	if (this->x == point.x && this->y == point.y){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 bool Point::operator != (const Point & point) const
 {
-	throw logic_error("Not Implmented yet");
-	return false; 
+	return !(operator==(point));
 }
